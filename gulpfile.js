dd5021700d90818eb3a39b1edb724fe5220b515d
Quentin Valmori <quentin.valmori@gmail.com>
2016-02-29 10:42:51 +0100

    current work

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var socketio = require('socket.io');
+var fs = require("fs");
+var exec = require('exec');
+
+//Need to create config file
+var io = socketio.listen('3002', function (err, msg) {
+    if (err) {
+        console.error(err);
+    }
+});
+
+var fileChanged = null;
+
+io.on('connection', function (socket) {
+    console.log('a client connected');
+    socket.on('disconnect', function () {
+        console.log('a client disconnected');
+    });
+    socket.on('received-content', function(data){
+        var filePathOnReader = data.path; 
+        var newFilePath = fileChanged.path;
+        
+        //Need a switch here
+        if(data.RS === 'readium'){
+            //Need to find out how to get the real path from Readium
+            var cmd = 'cat '+newFilePath+' > /Users/quentin/Sites/readium-viewer/readium-js-viewer/epub_content/myfile/OEBPS/Text/cover.xhtml';
+        }else {
+            var cmd = 'cat '+newFilePath+' >'+ filePathOnReader;    
+        }
+        
+        console.log('cmd');
+        console.log(cmd);
+
+        exec(cmd, function(err, out, code){
+            process.stderr.write(err);
+            process.stdout.write(out);
+            socket.emit('file_updated', {'RS': data.RS});
+        });
+        
+        console.log('contneu recu');
+        console.log(data);
+    });
+});
+
+
+
+
+
+    
+    gulp.task("default", function() {
+        gulp.watch('./epub/**/*.xhtml').on('change', function (file) {
+            console.log('file changed');
+            console.log(file);
+            fileChanged = file;
+            var fileContent = fs.readFileSync(file.path, "utf8");
+            io.sockets.emit('hot-reload', {
+                'file' : file,
+                'content' : fileContent
+                }
+            );
+        });
+    });
---
+++ libs/elr.js
@@ -0,0 +1,37 @@
+//needs config
+var socketHotReload = io.connect('http://127.0.0.1:3002');
+socketHotReload.on('file_updated', function(data){	
+	//console.log(data);
+	if(data.RS == 'ibooks'){
+		location.reload();
+	//currently else === Readium. it's an iframe
+	}else {
+		window.parent.location.href = window.parent.location.href;
+	}
+});
+
+socketHotReload.on('hot-reload', function (data) {
+		//ibooks
+		if(window.parent === window){
+			console.log("update");
+		      socketHotReload.emit('received-content',
+		      {
+		          'path' : window.location.pathname,
+		          'RS' : 'ibooks'
+		      }
+		   	 );                    
+		}else{
+			//READIUM
+			socketHotReload.emit('received-content',
+		      {
+		          'path' : window.location.pathname,
+		          'RS' : 'readium'
+		      });
+      //var newHTML = document.createElement('html');
+      //newHTML.textContent = data.content;
+      //window.parent.document.replaceChild(document.getElementsByTagName('html')[0], newHTML);
+     // window.parent.appendChild(newHTML);
+			//document.getElementsByTagName('html')[0].textContent = data.content;
+		}
+    //document.body.innerHTML = data;
+});
